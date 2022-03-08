786be2bb76979040b9083ff965bd04709d2905b7
Anthony Jackson <aj213@humboldt.edu>
2022-03-07 20:29:19 -0500

    This test works

--- test_calculate.py
+++ test_calculate.py
@@ -1,11 +1,12 @@
-import unittest
+import unittest 
 import SimpleCalculator
 
 class TestCalculate(unittest.TestCase):
     def setUp(self):
-        self.calc = Calculate()
+        self.calc = SimpleCalculator.Calculate()
     def test_add_method_returns_correct(self):
         self.assertEqual(4, self.calc.add(2,2))
+    
 
 if __name__ == '__main__':
     unittest.main()
